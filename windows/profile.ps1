fedcc3efe4d60e105ada580cfabd08fb969a4888
okshouiti <59249869+okshouiti@users.noreply.github.com>
2021-04-13 23:16:22 +0900

    Add multimedia utils

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -1,35 +1,57 @@
 ﻿# `~\OneDrive\ドキュメント\PowerShell\profile.ps1` includes this file
 
-$config_dir = "$HOME\OneDrive\asset\dotfiles"
+# 定数定義
+Set-Variable -Name DOTFILE_DIR `
+    -Value "$HOME\OneDrive\asset\dotfiles" `
+    -Option Constant `
+    -Scope Global `
+    -Description "設定ファイルが置いてあるディレクトリパス"
+Set-Variable -Name ENCODER_DIR `
+    -Value "$HOME\OneDrive\asset\encoder" `
+    -Option Constant `
+    -Scope Global `
+    -Description "エンコーダのバイナリが置いてあるディレクトリパス"
+
+#$config_dir = "$HOME\OneDrive\asset\dotfiles"
+#$encoder_dir = "$HOME\OneDrive\asset\encoder"
 
 # ウィンドウタイトル変更
 (Get-Host).UI.RawUI.WindowTitle = "PS Core"
 
 
 
+
+
 # ====================================================
 # ===============  プロンプトスタイル  ===============
 # ====================================================
+
 # Install-Module posh-git -Scope CurrentUser
 # Install-Module oh-my-posh -AllowPrerelease -Scope CurrentUser
 Import-Module posh-git
 Import-Module oh-my-posh
 #   posh3組み込みテーマ一覧  https://ohmyposh.dev/docs/themes
-Set-PoshPrompt (Join-Path -Path $config_dir -ChildPath "windows\posh3_theme_Dark.json")
+Set-PoshPrompt "$DOTFILE_DIR\windows\posh3_theme_Dark.json"
+
+
 
 
 
 # ====================================================
 # ======================  文字  ======================
 # ====================================================
+
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 $ENV:LESSCHARSET = "utf-8"
+
+
 
 
 
 # ====================================================
 # =================  便利プラグイン  =================
 # ====================================================
+
 # Get-ChildItemColor - 色付きls
 #   Install-Module -AllowClobber Get-ChildItemColor
 Import-Module Get-ChildItemColor
@@ -41,40 +63,91 @@ Invoke-Expression (& {
 })
 
 # bat - ハイライト付きcat
-function Read-FileWithBat($file) {
+function b($file) {
     & bat --theme=TwoDark $file
 }
-Set-Alias b Read-FileWithBat
 
-# stylus - CSSメタ言語
-function Compile-Stylus($target){
-    $node = "$HOME\scoop\apps\nodejs\current\node.exe"
-    $styl = "$HOME\scoop\apps\nodejs\current\bin\node_modules\stylus\bin\stylus"
-    & $node $styl -c $target
+# julialts - LTS版juliaを起動するコマンド
+function julialts() {
+    & "$HOME\AppData\Local\Julia-1.0.5\bin\julia.exe"
 }
-Set-Alias stylus Compile-Stylus
+
+function Rename-Sort {
+    $items = Get-ChildItem -File | Sort-Object {<#[int]#>$_.Basename}
+    $digits = ($items.Length -split '').Count - 2
+    Write-Host "Renamed"
+    for ($i=0; $i -lt $items.Length; $i++) {
+        $src = $items[$i].Name
+        $dst = ([string]($i+1)).PadLeft($digits, '0') + $items[$i].Extension
+        Rename-Item -Path $src -NewName $dst
+        Write-Host (" "*4 + $dst + "  from  " + $src)
+    }
+}
+
+
 
 
 
 # ====================================================
 # =================  マルチメディア  =================
 # ====================================================
-function Convert-PNGtoWEBP(){
+
+function webp() {
     $files = Get-ChildItem -File | Where-Object {$_.Extension -eq ".png"}
-    Foreach ($f in $files) {
+    foreach ($f in $files) {
         $src = $f.FullName
         $dest = "$($f.BaseName).webp"
         & cwebp -z 9 -m 6 -mt -noalpha -lossless -metadata none -progress $src -o $dest
     }
 }
-Set-Alias webp Convert-PNGtoWEBP
 
-function Convert-PNGtoHEIF(){
+
+function heic() {
     $files = Get-ChildItem -File | Where-Object {$_.Extension -eq ".png"}
-    $encoder = "$HOME\OneDrive\asset\encoder\heifenc.exe"
-    Foreach ($f in $files) {
+    foreach ($f in $files) {
         $src = $f.FullName
-        & $encoder --quality 55 -p x265:preset=placebo -p x265:tu-intra-depth=4 --no-alpha --no-thumb-alpha $src
+        & "$ENCODER_DIR\heifenc.exe" --quality 55 -p x265:preset=placebo -p x265:tu-intra-depth=4 --no-alpha --no-thumb-alpha $src
     }
 }
-Set-Alias heic Convert-PNGtoHEIF
+
+
+# png→avif変換
+# オプション → https://github.com/link-u/cavif/blob/master/doc/ja_JP/usage.md
+function avif() {
+    $files = Get-ChildItem -File | Where-Object {$_.Extension -eq ".png"}
+    foreach ($f in $files) {
+        $src = $f.Name
+        $dst = $f.BaseName + ".avif"
+        & "$ENCODER_DIR\cavif.exe" -i $src -o $dst `
+            --profile 1 `
+            --pix-fmt "yuv444" `
+            --bit-depth 8 `
+            --encoder-usage "good" `
+            --rate-control "q" `
+            --crf 20 `
+            --threads 12
+    }
+}
+
+
+function realsr($outExt = ".webp") {
+    $targets = @(".png", ".jpg", ".webp")
+    $files = Get-ChildItem -File | Where-Object {$_.Extension -in $targets}
+    foreach ($f in $files) {
+        if ($f.Extension -eq $outExt) {
+            $dest = "realsr___$($f.BaseName).$outExt"
+        } else {
+            $dest = "$($f.BaseName).$outExt"
+        }
+        & "$ENCODER_DIR\realsr\realsr-ncnn-vulkan.exe" -i $f.FullName -o $dest
+    }
+}
+
+
+function extract_frames($rate = "10") {
+    $targets = @(".mp4", ".mkv", ".webm")
+    $files = Get-ChildItem -File | Where-Object {$_.Extension -in $targets}
+    foreach ($f in $files) {
+        & ffmpeg -i $f.Name -r "1/$rate" -vcodec png "$($f.DirectoryName)\frames\%06d-$($f.BaseName).png"
+    }
+}
